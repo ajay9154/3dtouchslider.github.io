f0a2bd9ae01d4cdea18a705d486bd20a186c2c0c
Ajay Kumar Kushwaha <108119093+ajay9154@users.noreply.github.com>
2023-10-16 09:42:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/* YouTube - https://www.youtube.com/channel/UCCbMCkS3AwaAVXPSSLglIPQ  */
+
+/* YouTube - @devajaykumar */
+
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
